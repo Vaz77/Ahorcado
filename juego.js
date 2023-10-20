0b55496366a31dbcf6b38941a205e75898cfd43d
Jesus <jesusvazquez2477@gmail.com>
2023-10-20 20:46:22 +0200

    Implemento logica para detectar que letra es al hacer click y desabilitarla una vez adivinada, tambien implemento funcion para mostrar la letra en la posicion correcta en el elemento parrafo si está en la palabra

--- juego.js
+++ juego.js
@@ -1,3 +1,4 @@
+let palabrita;
 const palabras = [
   "manzanas",
   "camiseta",
@@ -13,12 +14,15 @@ btn.addEventListener("click", iniciar);
 console.log(btn);
 
 function iniciar(event) {
+  btn.disabled = true;
   const parrafo = id("palabra_a_adivinar");
+  parrafo.innerHTML = "";
+
   const cant_palabras = palabras.length;
   const valor_mas_bajo = 0; //indice mas bajo del array
   const valor_al_azar = obtener_random(0, cant_palabras);
 
-  const palabrita = palabras[valor_al_azar];
+  palabrita = palabras[valor_al_azar];
   console.log(palabrita);
   const cant_letras = palabrita.length;
 
@@ -26,4 +30,27 @@ function iniciar(event) {
     const span = document.createElement("span");
     parrafo.appendChild(span);
   }
+
+  const btn_letras = document.querySelectorAll("#letras button");
+  btn_letras.forEach(function (button) {
+    button.addEventListener("click", function () {
+      click_letras(button);
+    });
+  });
+}
+
+function click_letras(button) {
+  const letra = button.textContent;
+  if (palabrita.includes(letra)) {
+    // La letra está en la palabra, muestra la letra en la posición correcta
+    const letras = palabrita.split("");
+    const spans = document.querySelectorAll("#palabra_a_adivinar span");
+    for (let i = 0; i < letras.length; i++) {
+      if (letras[i] === letra) {
+        spans[i].textContent = letra;
+      }
+    }
+  }
+  // Deshabilita el botón después de hacer clic
+  button.disabled = true;
 }
