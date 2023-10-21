c07f07e89e76baef716810c3300e2fe5c207594f
Jesus <jesusvazquez2477@gmail.com>
2023-10-21 12:18:07 +0200

    Actualizo funcion de iniciar juego y la funcion de reiniciar juego, la lógica del juego funciona correctamente, corregido error al reiniciar el juego que duplicaba las imagenes y palabras

--- juego.js
+++ juego.js
@@ -1,6 +1,7 @@
 let palabrita;
 let cant_errores = 0;
 let cant_aciertos = 0;
+let juegoActivo = false;
 
 const palabras = [
   "manzanas",
@@ -15,37 +16,44 @@ const btn = id("jugar");
 const btn_letras = document.querySelectorAll("#letras button");
 const imagen = id("imagen");
 
-//click en iniciar juego
-btn.addEventListener("click", iniciar);
+btn.addEventListener("click", function () {
+  if (!juegoActivo) {
+    iniciar();
+  }
+});
 
 console.log(btn);
 
-function iniciar(event) {
+function iniciar() {
+  juegoActivo = true;
   imagen.src = "img/img0.png";
   btn.disabled = true;
+  id("resultado").innerHTML = "";
   const parrafo = id("palabra_a_adivinar");
   parrafo.innerHTML = "";
 
   const cant_palabras = palabras.length;
-  const valor_mas_bajo = 0; // índice más bajo del array
   const valor_al_azar = obtener_random(0, cant_palabras);
 
   palabrita = palabras[valor_al_azar];
   console.log(palabrita);
   const cant_letras = palabrita.length;
 
-  //click de adivinar letra
+  for (let i = 0; i < btn_letras.length; i++) {
+    btn_letras[i].disabled = false;
+  }
+
   for (let i = 0; i < cant_letras; i++) {
     const span = document.createElement("span");
     parrafo.appendChild(span);
   }
-
-  btn_letras.forEach(function (button) {
-    button.addEventListener("click", function () {
-      click_letras(button);
-    });
-  });
 }
+
+btn_letras.forEach(function (button) {
+  button.addEventListener("click", function () {
+    click_letras(button);
+  });
+});
 
 function click_letras(button) {
   const letra = button.textContent;
@@ -60,10 +68,8 @@ function click_letras(button) {
       }
     }
   }
-  // Deshabilita el botón después de hacer clic
   button.disabled = true;
 
-  // Verifica si se ha adivinado la palabra (fuera de la función)
   let acerto = false;
   for (let i = 0; i < palabrita.length; i++) {
     if (palabrita[i] === letra) {
@@ -72,10 +78,15 @@ function click_letras(button) {
     }
   }
 
-  if (acerto == false) {
+  if (!acerto) {
     cant_errores++;
-    const source = `img/img${cant_errores}.png`;
-    imagen.src = source;
+    if (cant_errores <= 7) {
+      const source = `img/img${cant_errores}.png`;
+      // Verificar si se han agotado los intentos
+      if (cant_errores <= 7) {
+        imagen.src = source;
+      }
+    }
   }
 
   if (cant_errores == 7) {
@@ -90,10 +101,35 @@ function click_letras(button) {
     "La letra " + letra + " en la palabra " + palabrita + " ¿existe?: " + acerto
   );
 }
-//fin del juego
+
+// Fin del juego
 function game_over() {
   for (let i = 0; i < btn_letras.length; i++) {
     btn_letras[i].disabled = true;
   }
   btn.disabled = false;
+
+  // Agrega un nuevo evento al boton "Obtener palabra" para reiniciar el juego
+  btn.addEventListener("click", reiniciarJuego);
+}
+
+// Función para reiniciar el juego
+function reiniciarJuego() {
+  // Elimina el evento "reiniciarJuego" del botón para evitar duplicados
+  btn.removeEventListener("click", reiniciarJuego);
+
+  for (let i = 0; i < btn_letras.length; i++) {
+    btn_letras[i].disabled = false;
+  }
+  btn.disabled = true;
+  id("resultado").innerHTML = "";
+  cant_errores = 0;
+  cant_aciertos = 0;
+
+  // Elimina los elementos de adivinanza de letras
+  const parrafo = id("palabra_a_adivinar");
+  parrafo.innerHTML = "";
+
+  // Llama a la función de inicio para comenzar un nuevo juego
+  iniciar();
 }
